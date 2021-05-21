19a31318ab62715ff820f01f6dce2f0581903059
ChengKeWen <790440671@qq.com>
2021-05-21 16:44:43 +0800

    修改文件FirstFilter.java

--- src/main/java/com/it/controller/SpringBootController.java
+++ src/main/java/com/it/controller/SpringBootController.java
@@ -4,12 +4,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
 @RestController
 public class SpringBootController {
 
     @RequestMapping("/getName")
     public Object getName(){
-        System.out.println("---------------getName----------------");
+
+        System.out.println("Name");
+
         return "getName";
     }
 }
